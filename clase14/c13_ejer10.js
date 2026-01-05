7f1e0dab71d5338c31af2d61c5ce88717b689532
Daniela <danusan14@hotmail.com>
2026-01-05 07:34:12 -0300

    agrego comentarios y detalles con la explicacion de los ejercicios en clase

--- clase14/c13_ejer10.js
+++ clase14/c13_ejer10.js
@@ -4,10 +4,10 @@ Fórmula del factorial:
 */
 
 let calcularFactorial = function(num){
-    let resultado = 1;
-    for (let i = num; i >= 1; i--){
-        resultado = resultado *i;
+    let resultado = 1;                  //Se inicializa la variable resultado en 1, factorial nunca empieza en 0.
+    for (let i = 1; i <= num; i++){     //El bucle se repite mientras i sea menor o igual a num
+        resultado = resultado *i;       //En cada iteración: se multiplica el valor actual de resultado por i. El resultado se va acumulando
     }
     return resultado
 } 
-console.log(calcularFactorial(5)); 
+console.log(calcularFactorial(4)); 
--- clase14/c13_ejer2.js
+++ clase14/c13_ejer2.js
@@ -5,8 +5,10 @@ Crea una función expresada llamada encontrarMayor que reciba dos números como 
 let encontrarMayor = function (num1, num2){
     if (num1 > num2){
         return num1;
-    } else {
+    } else if (num1 < num2){
         return num2;
+    } else{
+        return "son iguales"
     }
-}
+};
 console.log(encontrarMayor(1,5));
